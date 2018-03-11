e866613eae2c4988e0ceb4c4c5e13c5b85da9c0c
github username <github email address>
2018-03-10 23:44:43 -0500

    created navMenu component

---
+++ src/components/NavMenu.js
@@ -0,0 +1,41 @@
+// remove and fix styling
+
+
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+import Grid from 'material-ui/Grid';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import Toolbar from 'material-ui/Toolbar';
+import Logo from './Logo';
+
+const navIcon = {
+    marginLeft: 'auto',
+};
+
+const noBorder = {
+  'box-shadow': 'none'
+}
+
+class NavMenu extends React.Component {
+
+  render() {
+
+    return (
+      <Grid container spacing={24}>
+        <AppBar color = 'default' style = { noBorder }>
+          <Toolbar>
+            <Grid item xs={9} sm={6}>
+              <Logo></Logo>
+            </Grid>
+            <IconButton style = { navIcon }>
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </Grid>
+    )
+  }
+}
+
+export default NavMenu;
