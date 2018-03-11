b488463036e9578d8212da7d8323f36bf0ce86e6
github username <github email address>
2018-03-10 22:26:16 -0500

    created business logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Chameleon from '../images/Chameleon + OLORS + changingfinal.svg'
+
+const Logo = () => {
+  return (
+    <div>
+      <img src={ Chameleon } alt='logo' />
+    </div>
+  )
+}
+
+export default Logo;
