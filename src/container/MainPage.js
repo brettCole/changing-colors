a3917e9814531c292178cf2e194da3f0f62bbd17
github username <github email address>
2018-03-11 22:09:48 -0400

    created MainPage container

---
+++ src/container/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavMenu from '../components/NavMenu';
+
+class MainPage extends React.Component {
+  render() {
+    return (
+      <NavMenu></NavMenu>
+    )
+  }
+}
+
+export default MainPage;
